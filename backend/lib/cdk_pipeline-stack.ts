07ee96b755fd8c2cc5914c228d01a8629ce3e0d5
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-02-24 16:23:40 +0500

    backend

--- backend/lib/cdk_pipeline-stack.ts
+++ backend/lib/cdk_pipeline-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import * as ddb from "@aws-cdk/aws-dynamodb"
 
 export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const ddbTable = new ddb.Table(this , "ddbTable" , {
+      tableName : "Pipeline_table",
+      partitionKey : {
+        name : "id",
+        type  : ddb.AttributeType.STRING
+      }
+    })
+
   }
 }
